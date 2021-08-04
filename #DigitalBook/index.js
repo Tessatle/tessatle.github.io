f356a3f2ebde881fd40dd75878c9659012cb12e7
Tessatle <69765888+Tessatle@users.noreply.github.com>
2021-08-04 10:42:35 +0300

    f
    f

---
+++ #DigitalBook/index.js
@@ -0,0 +1,44 @@
+let logo=document.getElementById('feedback-logo'),
+	left=document.getElementById('left'),
+	right=document.getElementById('right'),
+	number=document.getElementById('number'),
+	namee=document.getElementById('namee'),
+	clientText=document.getElementById('clientText')
+
+function changeR(){
+	if(number.innerHTML=="01/"){
+		number.innerHTML="02/"
+		namee.innerHTML="ДАНИИЛ ФЕДОРОВ"
+		logo.src="img/DaniilFedorov.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	}else if(number.innerHTML=="02/"){
+		number.innerHTML="03/"
+		namee.innerHTML="ОЛЕГ БЕЛОЗЕРОВ"
+		logo.src="img/OlegBelozerov.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}else if(number.innerHTML=="03/"){
+		number.innerHTML="01/"
+		namee.innerHTML="ТИГРАН АНТОНЯН"
+		logo.src="img/TigranAntonyan.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}
+}
+
+function changeL(){
+	if(number.innerHTML=="01/"){
+		number.innerHTML="03/"
+		namee.innerHTML="ОЛЕГ БЕЛОЗЕРОВ"
+		logo.src="img/OlegBelozerov.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}else if(number.innerHTML=="03/"){
+		number.innerHTML="02/"
+		namee.innerHTML="ДАНИИЛ ФЕДОРОВ"
+		logo.src="img/DaniilFedorov.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	}else if(number.innerHTML=="02/"){
+		number.innerHTML="01/"
+		namee.innerHTML="ТИГРАН АНТОНЯН"
+		logo.src="img/TigranAntonyan.png"
+		clientText.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}
+}
